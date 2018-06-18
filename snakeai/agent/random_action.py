c33c32814407bbe7c2c21e3e1aabe10f8d8772e2
José Chu <jchudb93@gmail.com>
2018-06-17 23:02:47 -0500

    snake env

---
+++ snakeai/agent/random_action.py
@@ -0,0 +1,19 @@
+import random
+import numpy as np
+from snakeai.agent import AgentBase
+from snakeai.gameplay.entities import ALL_SNAKE_ACTIONS
+
+class RandomActionAgent(AgentBase):
+    """ Represents a Snake agent that takes a random action at every step. """
+    
+    def __init__(self):
+        pass
+
+    def begin_episode(self):
+        pass
+
+    def act(self, observation, reward):
+        return np.random.choice(ALL_SNAKE_ACTIONS)
+
+    def end_episode(self):
+        pass
